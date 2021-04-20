576427e810bd016b5908163cbc55da07ab778dbf
Justin Peczenij <justinpeczenij@gmail.com>
2021-04-20 18:42:19 -0400

    friend is posted to backend

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,7 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
 function FriendsList() {
-    return <h3>FriendsList</h3>
+    const [formValues, setFormValues] = useState({
+        name: '',
+        age: '',
+        email: '',
+        id: ''
+    })
+
+    const handleChanges = (e) => {
+        setFormValues({
+            ...formValues,
+            [e.target.name]: e.target.value
+        })
+    }
+    
+    const postFriendToDB = (e) => {
+        e.preventDefault();
+        axiosWithAuth()
+            .post('/api/friends', formValues)
+            .then(res => console.log(res))
+            .catch(err => console.log(err))
+    }
+
+    return (
+        <div>
+            <form onSubmit={postFriendToDB} >
+                <legend>Add a Friend</legend>
+                <input 
+                    name='name'
+                    placeholder='name'
+                    type='text'
+                    value={formValues.name}
+                    onChange={handleChanges}
+                />
+                    <input 
+                        name='email'
+                        placeholder='email'
+                        type='email'
+                        value={formValues.email}
+                        onChange={handleChanges}
+                    />
+                <input 
+                    name='age'
+                    placeholder='age'
+                    type='number'
+                    value={formValues.age}
+                    onChange={handleChanges}
+                />
+                <button type='submit'>Add a Friend!</button>
+            </form>
+        </div>
+    )
 }
 
 export default FriendsList;
